ced991e4eae278df509f0890b3377cc59ece4227
Jhonathan Freitas <161077417+JhonathanFreitas@users.noreply.github.com>
2024-05-04 15:30:23 -0300

    Add files via upload

---
+++ Modelo - Questão.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    question: "Qual é o principal propósito da linguagem de marcação HTML?",
+    answers: [
+      { option: "Estilizar páginas da web", correct: false },
+      { option: "Gerenciar bancos de dados", correct: false },
+      { option: "Criar a estrutura de uma página da web", correct: true },
+      { option: "Programar algoritmos complexos", correct: false },
+    ],
+  },
+  {
+    question: "O que significa CSS em desenvolvimento web?",
+    answers: [
+      { option: "Computer System Style", correct: false },
+      { option: "Cascading Style Sheets", correct: true },
+      { option: "Creative Style System", correct: false },
+      { option: "Coding Style Syntax", correct: false },
+    ],
+  },
+  {
+    question: "O que é um algoritmo?",
+    answers: [
+      { option: "Um programa de computador.", correct: false },
+      { option: "Um dispositivo de hardware.", correct: false },
+      { option: "Uma sequência de instruções passo a passo para resolver um problema.", correct: true },
+    ],
+  },
+  {
+    question: "O que é a Teoria da Computação?",
+    answers: [
+      { option: "O estudo dos algoritmos e estruturas de dados.", correct: false },
+      { option: "O estudo das redes de computadores.", correct: false },
+      { option: "O estudo da computação e seus limites.", correct: true },
+    ],
+  },
+  {
+    question: "O que é um diagrama de fluxo?",
+    answers: [
+      { option: "Uma representação visual de um algoritmo, mostrando as etapas do processo e a ordem em que elas ocorrem", correct: true },
+      { option: "Um tipo de gráfico usado para representar a estrutura de um banco de dados.", correct: false },
+      { option: "Uma ferramenta de edição de imagens.", correct: false },
+    ],
+  },
+];
